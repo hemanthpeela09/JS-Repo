e18f3687e8d12661ffb11c1c376756b62c719c0f
Hemanth Peela <Hemanthpeela@Hemanths-MacBook-Air.local>
2017-02-23 23:57:26 +0530

    function is an Object
    In JS function is an Object and can add Properties which is not
    possible in any other language.

--- pocJS/script1.js
+++ pocJS/script1.js
@@ -10,3 +10,14 @@ english.greet = "Hello";
 spanish.greet = "Hola";
 
 console.log(english);
+
+function cheer(){
+    console.log("Hi there!!");
+}
+
+cheer.language = "english";
+console.log(cheer); //In JS function is also an Object
+
+console.log(cheer.language); //Function Property
+
+cheer(); //Function Invocation
