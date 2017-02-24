705a21a4421ee213241b94573590b01e871ab858
Hemanth Peela <Hemanthpeela@Hemanths-MacBook-Air.local>
2017-02-24 23:00:25 +0530

    Function & its Statements
    Function can be an Object in JS

--- pocJS/script1.js
+++ pocJS/script1.js
@@ -21,3 +21,25 @@ console.log(cheer); //In JS function is also an Object
 console.log(cheer.language); //Function Property
 
 cheer(); //Function Invocation
+
+
+var anonymousFunc = function(){
+    console.log("In Anonymous Function"); 
+}
+
+anonymousFunc();
+
+function anyValue(a){
+    console.log(a);
+}
+
+anyValue("Any value dynamically");
+anyValue(5);
+
+function anyFunc(a){
+    a();
+}
+
+anyFunc(function(){
+   console.log("Any Function");
+});
